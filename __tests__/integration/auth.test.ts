cc5d021619823f5c404b6a2992f9544ee1709d72
Guilherme Hendres <ghendressmelo@gmail.com>
2022-02-22 17:57:42 -0300

    inicio da configuaracao dos testes

---
+++ __tests__/integration/auth.test.ts
@@ -0,0 +1,14 @@
+describe('authenticate', () => {
+    it('should return jwt token when valid credentials', () => {
+        const x = 1
+        const y = 3
+
+        const sum = x + y
+        expect(sum).toBe(4)
+
+    })
+})
+
+describe('register', () => {
+
+})
